0c9b80c0de17fe24748a315bcb71ca561d3df3ac
nicolasalarconrapela <nicolasalarconrapela@users.noreply.github.com>
2026-01-09 23:06:09 +0100

    Añadir repo de FFmpegKit para CI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.devhyper.openvideoeditor"
         minSdk = 26
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.1.3"
+        versionCode = 15
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
