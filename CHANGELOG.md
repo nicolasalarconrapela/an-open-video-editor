36ec04e74a296453edf31bd00a70eff6651f9e53
nicolasalarconrapela <nicolasalarconrapela@users.noreply.github.com>
2026-01-09 22:40:58 +0100

    Añadir workflow APK debug para develop

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## [1.2.0] - 2026-01-09
+### Añadido
+- Workflow de GitHub Actions para generar APK debug en la rama develop.
+
 ## [1.1.9] - 2026-01-09
 ### Añadido
 - Tests unitarios para exportación y planificación de segmentos.
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.devhyper.openvideoeditor"
         minSdk = 26
         targetSdk = 34
-        versionCode = 13
-        versionName = "1.1.9"
+        versionCode = 14
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
